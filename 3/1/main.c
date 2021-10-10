05f888c9b9a64c0c9e55c179538a9acf2e976910
Dmytro <dimasoltusyuk@gmail.com>
2021-10-10 16:37:35 +0300

    3.1 done

--- 3/1/main.c
+++ 3/1/main.c
@@ -1,4 +1,38 @@
+#include <math.h>
+#include <stdio.h>
+
 int main(void)
 {
+    unsigned int argsNum;
+
+    printf("Enter number of arguments (minimum - 2): ");
+    scanf("%d", &argsNum);
+
+    if (argsNum < 2)
+    {
+        puts("Vector should have minimum 2 elements to proceed");
+        return 1;
+    }
+
+    unsigned int vector[argsNum], formattedVector[argsNum / 2];
+    unsigned int index = 0;
+
+    for (int i = 0; i < argsNum; i++)
+    {
+        printf("Enter number for %d array position: ", i);
+        scanf("%d", &vector[i]);
+
+        if (i >= 1)
+        {
+            formattedVector[index++] = sqrt(vector[i] * vector[i - 1]);
+        }
+    }
+
+    puts("New vector:");
+    for (int i = 0; i < index; i++)
+    {
+        printf("%d ", formattedVector[i]);
+    }
+
     return 0;
-}   
+}
