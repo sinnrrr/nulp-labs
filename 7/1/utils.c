7ea00e51ad3e132a5cd104a7bf4ba1f4fcb1f1bc
Dmytro <dimasoltusyuk@gmail.com>
2021-12-12 02:16:20 +0200

    8.1: proper array initializing

--- 7/1/utils.c
+++ 7/1/utils.c
@@ -1,10 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 
-void fillArraysWithRandomItems(long size, long arr1[], long arr2[]) {
+char *asd[];
+
+void fillArraysWithRandomItems(long size, long arr1[], long arr2[])
+{
     srand(time(NULL));
 
-    for (long i = 0; i < size; i++) {
+    for (long i = 0; i < size; i++)
+    {
         long randomNumber = rand();
 
         arr1[i] = randomNumber;
@@ -12,25 +16,33 @@ void fillArraysWithRandomItems(long size, long arr1[], long arr2[]) {
     };
 }
 
-void cloneArray(const long size, long from[], long to[]) {
-    for (long i = 0; i <= size; i++) {
+void cloneArray(const long size, long from[], long to[])
+{
+    for (long i = 0; i <= size; i++)
+    {
         to[i] = from[i];
     }
 }
 
-long areSame(const long size, const long arr1[], const long arr2[]) {
+long areSame(const long size, const long arr1[], const long arr2[])
+{
     long sameItemsCount = 0;
 
-    for (long i = 0; i < size; ++i) {
-        if (arr1[i] == arr2[i]) sameItemsCount++;
+    for (long i = 0; i < size; ++i)
+    {
+        if (arr1[i] == arr2[i])
+            sameItemsCount++;
     }
 
     return sameItemsCount == size;
 }
 
-long arrayIsOrdered(const long size, const long arr[]) {
-    for (long i = 1; i < size; ++i) {
-        if (arr[i] < arr[i - 1]) return 0;
+long arrayIsOrdered(const long size, const long arr[])
+{
+    for (long i = 1; i < size; ++i)
+    {
+        if (arr[i] < arr[i - 1])
+            return 0;
     }
 
     return 1;
---
+++ 7/2/main.c
@@ -0,0 +1,59 @@
+#include <math.h>
+#include <stdio.h>
+
+void tabulate(double x) {
+    double y = pow(cos(x), 4);
+
+    printf("% .10lf\t% .10lf\t\n", x, y);
+}
+
+void usingFor(double start, double step, double end) {
+    for (double x = start; x < end; x += step) tabulate(x);
+}
+
+void usingWhile(double start, double step, double end) {
+    double x = start;
+    while (end > x) {
+        tabulate(x);
+
+        x += step;
+    }
+}
+
+void usingDoWhile(double start, double step, double end) {
+    double x = start;
+    do {
+        tabulate(x);
+
+        x += step;
+    } while (end > x);
+}
+
+int main(void) {
+    void (*functions[])(
+            double start,
+            double step,
+            double end
+    ) = {&usingFor, &usingWhile, &usingDoWhile};
+
+    double step, start, end;
+
+    int methodToUse = 0;
+    int methodsAvailableCount = sizeof functions / sizeof functions[0];
+
+    printf("Enter start, step and the end: ");
+    scanf("%lf %lf %lf", &start, &step, &end);
+
+    printf("Enter method to use: ");
+    scanf("%d", &methodToUse);
+
+    if (methodToUse > methodsAvailableCount) {
+        puts("We have any definition of that method number!");
+
+        return 1;
+    }
+
+    functions[methodToUse](start, step,  end);
+
+    return 0;
+}
