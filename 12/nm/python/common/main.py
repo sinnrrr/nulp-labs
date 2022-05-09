73b08395dd6593fdccf33df099e5e4902f0753b5
Dmytro Soltysiuk <dimasoltysiuk@gmail.com>
2022-05-09 20:28:40 +0200

    feat: lab 8 utils file

--- 12/nm/python/common/main.py
+++ 12/nm/python/common/main.py
@@ -1,28 +1,10 @@
 from math import fabs
 
 import numpy as np
-from colorama import Fore, Style
+from common.utils import print_header, print_matrix, print_method_introduction
 
 Row = list[float]
 Matrix = list[Row]
-
-
-def print_matrix(matrix, name: str):
-    s = [[str(e) for e in row] for row in matrix]
-    lens = [max(map(len, col)) for col in zip(*s)]
-    fmt = "\t".join("{{:{}}}".format(x) for x in lens)
-    table = [fmt.format(*row) for row in s]
-
-    print(f"\n{name}:")
-    print("\n".join(table))
-
-
-def print_header(header: str):
-    print(Fore.RED + header + Fore.RESET)
-
-
-def print_method_introduction(name: str):
-    print(f"{Fore.CYAN}>>> {name} method{Style.RESET_ALL}\n")
 
 
 class Method:
---
+++ 12/nm/python/common/utils.py
@@ -0,0 +1,19 @@
+from colorama import Fore, Style
+
+
+def print_matrix(matrix, name: str):
+    s = [[str(e) for e in row] for row in matrix]
+    lens = [max(map(len, col)) for col in zip(*s)]
+    fmt = "\t".join("{{:{}}}".format(x) for x in lens)
+    table = [fmt.format(*row) for row in s]
+
+    print(f"\n{name}:")
+    print("\n".join(table))
+
+
+def print_header(header: str):
+    print(Fore.RED + header + Fore.RESET)
+
+
+def print_method_introduction(name: str):
+    print(f"{Fore.CYAN}>>> {name} method{Style.RESET_ALL}\n")
