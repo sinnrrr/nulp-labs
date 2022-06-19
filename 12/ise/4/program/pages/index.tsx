0d67a9623d92a440016150d55eca1b0673bacb09
Dmytro Soltysiuk <dimasoltysiuk@gmail.com>
2022-06-19 21:15:42 +0200

    feat(ise): updates to rows

--- 12/ise/4/program/pages/index.tsx
+++ 12/ise/4/program/pages/index.tsx
@@ -1,12 +1,13 @@
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import DocumentScannerIcon from '@mui/icons-material/DocumentScanner';
 import { Box, Button, IconButton, Pagination, Stack, Typography } from '@mui/material';
-import { DataGrid, getGridDefaultColumnTypes, GridCellParams, GridColDef, GridFilterItem, GridFilterOperator, gridPageCountSelector, gridPageSelector, GridStateColDef, GridValueGetterParams, useGridApiContext, useGridSelector } from '@mui/x-data-grid';
+import { DataGrid, getGridDefaultColumnTypes, GridCellParams, GridColDef, GridFilterItem, GridFilterOperator, gridPageCountSelector, gridPageSelector, GridPreProcessEditCellProps, GridStateColDef, GridValueGetterParams, useGridApiContext, useGridSelector } from '@mui/x-data-grid';
 import { randFirstName, randLastName, randNumber, randPastDate } from '@ngneat/falso';
 import type { NextPage } from 'next';
+import { z } from 'zod';
 import { LoadStudentsFileButton } from '../components/LoadStudentsFileButton';
 import { useStudentsContext } from "../contexts/students";
-import { Student } from '../schemas/student';
+import { Student, StudentSchema } from '../schemas/student';
 import { averageNumber } from '../utils/averageNumber';
 
 const wrapOperator = (operator: GridFilterOperator) => {
@@ -36,26 +37,50 @@ const gridStringColumnDefaults = getGridDefaultColumnTypes().string
 const notContainsOperator = wrapOperator(gridStringColumnDefaults.filterOperators!.filter(f => f.value === "contains")[0])
 gridStringColumnDefaults.filterOperators!.unshift(notContainsOperator)
 
+const parseField = (field: keyof z.infer<typeof StudentSchema>) => {
+  return (params: GridPreProcessEditCellProps) => {
+    const data = StudentSchema.shape[field].safeParse(params.props.value)
+    const hasErrors = !data.success
+
+    return { ...params.props, error: hasErrors }
+  }
+}
+
 const columns: GridColDef<Student>[] = [
-  { field: 'firstName', headerName: 'First name' },
-  { field: 'lastName', headerName: 'Last name' },
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    editable: true,
+    preProcessEditCellProps: parseField("firstName")
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    editable: true,
+    preProcessEditCellProps: parseField("lastName")
+  },
   {
     field: 'grades',
     headerName: 'Grades',
-    filterOperators: gridStringColumnDefaults.filterOperators
+    editable: true,
+    valueParser: (value, _params) => value.split(',').map((v: string) => v ? parseInt(v) : ''),
+    filterOperators: gridStringColumnDefaults.filterOperators,
+    preProcessEditCellProps: parseField("grades"),
   },
   {
     field: 'gradesAverage',
     headerName: 'Average grade',
-    valueGetter: (params: GridValueGetterParams) => averageNumber(params.row.grades).toFixed(2)
+    valueGetter: (params: GridValueGetterParams) => averageNumber(params.row.grades).toFixed(2),
   },
   {
     field: 'birthDate',
     headerName: 'Birth date',
     width: 230,
+    editable: true,
     type: 'date',
+    preProcessEditCellProps: parseField("birthDate"),
     valueGetter: (params) => new Date(params.row.birthDate),
-    valueFormatter: (params) => params.value.toUTCString()
+    valueFormatter: (params) => params.value.toUTCString(),
   },
 ];
 
@@ -117,18 +142,27 @@ const CustomNoRowsOverlay = () => (
 )
 
 const Home: NextPage = () => {
-  const { students } = useStudentsContext()
+  const { students, setStudents } = useStudentsContext()
   const elementsPerPage = 20
 
   return (
     <Box sx={{ display: 'flex', minHeight: '100vh', flexGrow: 1 }}>
       <Box sx={{ display: 'flex', flexGrow: 1, width: "100%" }}>
         <DataGrid
+          experimentalFeatures={{ newEditingApi: true }}
           rows={students || []}
           columns={columns}
           pageSize={elementsPerPage}
           rowsPerPageOptions={[elementsPerPage]}
-          checkboxSelection
+          processRowUpdate={(data) => {
+            const studentsCopy = [...students || []]
+            const changedStudentIndex = studentsCopy.findIndex((el) => el.id === data.id)
+
+            studentsCopy[changedStudentIndex] = data
+            setStudents(studentsCopy)
+
+            return data
+          }}
           components={{ Pagination: CustomPagination, NoRowsOverlay: CustomNoRowsOverlay }}
         />
       </Box>
