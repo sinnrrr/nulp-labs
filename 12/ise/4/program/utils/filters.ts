8486aa6a61cd5d8b7c5974ed60f8bac42b99028f
Dmytro Soltysiuk <dimasoltysiuk@gmail.com>
2022-06-25 18:45:30 +0200

    fix(ise): lab 4 non reachable filter

--- 12/ise/4/program/utils/filters.ts
+++ 12/ise/4/program/utils/filters.ts
@@ -1,6 +1,10 @@
 import { Book, BookFilter } from "../schemas/book";
 
 export const filters: { [key in BookFilter]: (input: Book[]) => Book[] } = {
-  publishedEarlier1975: (books) => books.filter((book) => book.publishedAt.getFullYear() < 1975),
-  excludePublishedWithinLast5Years: (books) => books.filter((book) => book.publishedAt.getFullYear() > (new Date()).getFullYear() - 50)
-}
+  publishedEarlier1975: (books) =>
+    books.filter((book) => book.publishedAt.getFullYear() < 1975),
+  excludePublishedWithinLast50Years: (books) =>
+    books.filter(
+      (book) => book.publishedAt.getFullYear() > new Date().getFullYear() - 50
+    ),
+};
